2e97aefa9a84eed10d3d4657da18e6197bafa9d2
tooPieElf <ndimnezechukwudi@gmail.com>
2020-03-04 18:27:13 +0100

    add pathVariable

--- src/main/java/com/webservices/microservices/Helloworld.java
+++ src/main/java/com/webservices/microservices/Helloworld.java
@@ -1,7 +1,7 @@
 package com.webservices.microservices;
 
 import org.springframework.web.bind.annotation.GetMapping;
-import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RestController;
 
 @RestController
@@ -16,5 +16,9 @@ public class Helloworld {
     return new HelloworldBean("omega pasky is a good boy");
   }
 
+  @GetMapping("/world-bean/{chukwudi}")
+  public HelloworldBean helloPath(@PathVariable String chukwudi){
+    return new HelloworldBean(String.format("he is a good boy %s", chukwudi));
+  }
 
 }
