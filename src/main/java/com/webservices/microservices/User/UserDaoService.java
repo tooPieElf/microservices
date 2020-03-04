17cf255f5ba9715d6819adf993a5096258d14181
tooPieElf <ndimnezechukwudi@gmail.com>
2020-03-04 18:57:35 +0100

    add findOne method

--- src/main/java/com/webservices/microservices/User/UserDaoService.java
+++ src/main/java/com/webservices/microservices/User/UserDaoService.java
@@ -12,12 +12,13 @@ public class UserDaoService {
   static {
     users.add(new User("Chukwudi", 1, new Date()));
     users.add(new User("Obioma", 2, new Date()));
-    users.add(new User("Ebuka", 1, new Date()));
+    users.add(new User("Ebuka", 3, new Date()));
 
   }
   public List<User> findAll(){
     return users;
   }
+
   public User save(User user){
     if(user.getId()==0){
       user.setId(++userCount);
@@ -25,6 +26,14 @@ public class UserDaoService {
     users.add(user);
     return user;
 
+  }
+  public User findOne(int id){
+    for (User user: users) {
+      if(user.getId()==id){
+        return user;
+      }
+    }
+    return null;
   }
 
 
