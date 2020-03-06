687c88fddc739a246363f851ae7e8fc275a4c095
tooPieElf <ndimnezechukwudi@gmail.com>
2020-03-06 07:09:42 +0100

    Add custom exception handling

--- src/main/java/com/webservices/microservices/exceptions/CustomizedResponseEntityExceptionhandler.java
+++ src/main/java/com/webservices/microservices/exceptions/CustomizedResponseEntityExceptionhandler.java
@@ -1,5 +1,6 @@
 package com.webservices.microservices.exceptions;
 
+import com.webservices.microservices.User.UserNotFoundException;
 import java.util.Date;
 import org.springframework.http.HttpStatus;
 import org.springframework.http.ResponseEntity;
@@ -18,6 +19,13 @@ extends ResponseEntityExceptionHandler {
      ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription
           (false));
    return   new ResponseEntity(exceptionResponse, HttpStatus.INTERNAL_SERVER_ERROR);
+  }
+  @ExceptionHandler(UserNotFoundException.class)
+  public final ResponseEntity<Object> handleUserNotFoundException
+      (UserNotFoundException ex, WebRequest request){
+    ExceptionResponse exceptionResponse= new ExceptionResponse(new Date(), ex.getMessage(), request.getDescription
+        (false));
+    return   new ResponseEntity(exceptionResponse, HttpStatus.NOT_FOUND);
   }
 
 }
